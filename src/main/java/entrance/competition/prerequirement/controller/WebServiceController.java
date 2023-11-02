fa8a1f07b6e42d40ae91c77557e9fd561583d237
Mahila <mahila.hosseini@yahoo.com>
2023-11-02 11:39:58 +0100

    initial configuration

---
+++ src/main/java/entrance/competition/prerequirement/controller/WebServiceController.java
@@ -0,0 +1,13 @@
+package entrance.competition.prerequirement.controller;
+
+import entrance.competition.prerequirement.PreRequirementApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class WebServiceController {
+    private static Logger logger = LoggerFactory.getLogger(PreRequirementApplication.class);
+
+
+   }
