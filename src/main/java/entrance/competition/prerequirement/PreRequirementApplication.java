56a414a10534a8306fd55395208fa3b856a8a4f7
Mahila <mahila.hosseini@yahoo.com>
2023-11-02 10:59:54 +0100

    initial commit

---
+++ src/main/java/entrance/competition/prerequirement/PreRequirementApplication.java
@@ -0,0 +1,13 @@
+package entrance.competition.prerequirement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreRequirementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PreRequirementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/entrance/competition/prerequirement/PreRequirementApplicationTests.java
@@ -0,0 +1,13 @@
+package entrance.competition.prerequirement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreRequirementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
